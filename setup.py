caa9251ab1a7cc8673a4a3116e4f750aec711328
meiirbek.islamov <mei12@pitt.edu>
2020-10-12 11:23:55 -0400

    remove random in requirments.txt

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-setup(name='defectMOF',
+from setuptools import setup, find_packages
+
+setup(name='defects',
       version='0.1.0',
       description='Introducing Missing-linker Defects in Metal-Organic Frameworks',
       author='Meiirbek Islamov',
       author_email='mei12@pitt.edu',
       url='https://github.com/meiirbek-islamov/defects',
-      packages=['defectMOF'],
+      packages=find_packages(include=['defects']),
       install_requires=[
           'numpy',
-          'click',
-          'matplotlib',
-          'random',
-          're',
+          'matplotlib'
       ],
-      )
+)
