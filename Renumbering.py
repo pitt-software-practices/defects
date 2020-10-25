caf4d1e7ffe6a1caca57e01ff0004a4b5bd0f301
meiirbek.islamov <mei12@pitt.edu>
2020-10-25 11:34:29 -0400

    PEP8 changes: changed variable from l to cif_line in Introduce_Defects.py

--- Renumbering.py
+++ Renumbering.py
@@ -5,16 +5,16 @@ import re
 input = open("missing_linkers_final.txt", 'r')
 i = 0
 for line in input:
-    l = line.split()
-    if len(l) == 8:
+    L = line.split()
+    if len(L) == 8:
         i = i + 1
 natoms = i                               # number of atoms
 input.close()
 input = open("missing_linkers_final.txt", 'r')
 i = 0
 for line in input:
-    l = line.split()
-    if len(l) == 5:
+    L = line.split()
+    if len(L) == 5:
         i = i + 1
 nbonds = i                               # number of bonds
 input.close()
@@ -23,10 +23,10 @@ atoms_index = [0] * natoms
 input = open("missing_linkers_final.txt", 'r')
 j = -1
 for line in input:
-    l = line.split()
-    if len(l) == 8:
+    L = line.split()
+    if len(L) == 8:
         j = j + 1
-        s = re.split('Cu|C|O|H|',l[0])
+        s = re.split('Cu|C|O|H|',L[0])
         atoms_index[j] = int(float(s[1]))
 input.close()
 
@@ -40,11 +40,11 @@ bond_2 = [0] * nbonds
 input = open("missing_linkers_final.txt", 'r')
 j = -1
 for line in input:
-    l = line.split()
-    if len(l) == 5:
+    L = line.split()
+    if len(L) == 5:
         j = j + 1
-        s1 = re.split('Cu|C|O|H|',l[0])
-        s2 = re.split('Cu|C|O|H|',l[1])
+        s1 = re.split('Cu|C|O|H|',L[0])
+        s2 = re.split('Cu|C|O|H|',L[1])
         bond_1[j] = int(float(s1[1]))
         bond_2[j] = int(float(s2[1]))
 input.close()
@@ -63,10 +63,10 @@ atoms_symbols = [0] * natoms
 input = open("missing_linkers_final.txt", 'r')
 j = -1
 for line in input:
-    l = line.split()
-    if len(l) == 8:
+    L = line.split()
+    if len(L) == 8:
         j = j + 1
-        atoms_symbols[j] = str(l[1])
+        atoms_symbols[j] = str(L[1])
 input.close()
 
 i = -1
@@ -86,21 +86,21 @@ with open("missing_linkers_final.txt") as input:
     lines = input.readlines()
     with open("missing_linkers_renumbered.cif","w") as missing_linkers:
         for line in lines:
-            l = line.split()
+            L = line.split()
             k = k + 1
             if k < 25:
                 missing_linkers.write(line)
             elif k >= 25 and k < 25 + natoms:
-                if len(l) == 8:
+                if len(L) == 8:
                     i = i + 1
-                    missing_linkers.write(l[1] + str(atoms_index_new[i]) + "   " + l[1] + "   "
-                              + l[2] + "   " + l[3] + "   "
-                              + l[4] + "   " + l[5] + "   " + l[6] + "   " + l[7] + "\n")
+                    missing_linkers.write(L[1] + str(atoms_index_new[i]) + "   " + L[1] + "   "
+                              + L[2] + "   " + L[3] + "   "
+                              + L[4] + "   " + L[5] + "   " + L[6] + "   " + L[7] + "\n")
             elif k >= 25 + natoms and k < 31 + natoms:
                     missing_linkers.write(line)
             elif k >= 31 + natoms:
-                if len(l) == 5:
+                if len(L) == 5:
                     j = j + 1
                     missing_linkers.write(bond_1_symbols[j] + str(bond_1_new[j]) + "   "
-                                      + bond_2_symbols[j] + str(bond_2_new[j]) + "   " + l[2] + "   "
-                                     + l[3] + "   " + l[4] + "\n")
+                                      + bond_2_symbols[j] + str(bond_2_new[j]) + "   " + L[2] + "   "
+                                     + L[3] + "   " + L[4] + "\n")
